59dc764fb636a93a99442a5316b39d421d014ef4
kh4ngnx <nguyen2004xuankhang@gmail.com>
2026-02-25 10:49:25 +0700

    feat: add router for pages
    - /about
    - /skills
    - /projects
    - /education

---
+++ src/layouts/AppLayout.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import Navbar from '@/components/Navbar.vue';
+</script>
+
+<template>
+    <div class="layout">
+        <header>
+            <Navbar />            
+        </header> 
+
+        <main>
+            <router-view />
+        </main>
+    </div>
+</template>
+
+<style scoped>
+.layout {
+}
+</style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,45 @@
+import AppLayout from "@/layouts/AppLayout.vue";
+import About from "@/pages/About.vue";
+import Education from "@/pages/Education.vue";
+import Projects from "@/pages/Projects.vue";
+import Skills from "@/pages/Skills.vue";
 import { createRouter, createWebHistory } from "vue-router";
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
-    routes: [],
+    routes: [
+        {
+            path: "/",
+            component: AppLayout,
+            children: [
+                { path: "", redirect: "/about" },
+                {
+                    path: "about",
+                    component: About,
+                    meta: { tab: "about" },
+                },
+                {
+                    path: "skills",
+                    component: Skills,
+                    meta: { tab: "skills" },
+                },
+                {
+                    path: "projects",
+                    component: Projects,
+                    meta: { tab: "projects" },
+                },
+                {
+                    path: "education",
+                    component: Education,
+                    meta: { tab: "education" },
+                },
+            ],
+        },
+        {
+            path: "/:pathMath(.*)*",
+            redirect: "/about",
+        },
+    ],
 });
 
 export default router;
